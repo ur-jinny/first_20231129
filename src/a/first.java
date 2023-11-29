15b1bf7e75d9993ef793442756359ac5b0bfacca
ur-jinny <youjin4508@gmail.com>
2023-11-29 15:39:41 +0900

    first commit

---
+++ src/a/first.java
@@ -0,0 +1,16 @@
+// 현재 클래스(자바파일)가 소속된 패키지의 이름
+package a;
+
+// 현재 클래스의 이름
+public class first { // First class 블록의 시작
+
+    // main 매서드(프로그램의 시작 지점)
+	public static void main(String[] args) {// main 메서드 블록의 시작
+		// 실행할 내용이 작성되는 공간
+		
+		// 텍스트 또는 변수값을 출력할 수 있는 println() 메서드
+        System.out.println("자바야 앞으로 친하게 지내자..");
+	} // main 메서드 블록의 끝
+	
+} // First class 블록의 끝
+
